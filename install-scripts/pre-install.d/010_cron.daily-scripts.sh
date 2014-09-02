836d030a52354a9ec9f8794da0f808097ec352c9
Olivier Cortès <oc@1flow.io>
2014-09-02 18:37:30 +0200

    No need for the global & manual cron.daily/* installation, it's now a setting in $machine/install file.

--- install-scripts/pre-install.d/010_cron.daily-scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-for BINARY in ${GLOCONF_PATH}/cron.daily/*
-do
-    BASENAME=`basename ${BINARY}`
-    rm -f /etc/cron.daily/${BASENAME}
-    ln -sf ${BINARY} /etc/cron.daily/${BASENAME}
-done
