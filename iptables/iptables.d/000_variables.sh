b9cc8c606c3d147952666af70b35d787998c56c3
Olivier Cortès <oc@1flow.io>
2014-09-02 18:40:47 +0200

    firewall: move global variables to run.sh, and don't run if iptables is not installed (in an LXC) or if the main IP is a private one (any case).

--- iptables/iptables.d/000_variables.sh
+++ iptables/iptables.d/000_variables.sh
@@ -6,17 +6,6 @@
 #       wget -qqO - http://myip.dnsomatic.com
 #
 
-MAIN_IFACE="eth0"
-HOSTNAME=`hostname`
-SHORT_HOSTNAME=`hostname -s`
-IP_CACHE="/var/cache/myip.txt"
-
-if [ -f $IP_CACHE ]; then
-    MAIN_IP=`cat $IP_CACHE`
-else
-    MAIN_IP=`wget -qqO - http://myip.dnsomatic.com`
-    echo $MAIN_IP > $IP_CACHE
-fi
 
 if [ -e /etc/physconf/iptables/variables ]; then
 
--- iptables/run.sh
+++ iptables/run.sh
@@ -3,6 +3,32 @@
 #IPTABLES_ROOT="/home/groups/local_config/iptables"
 IPTABLES_ROOT=`dirname $0`
 
+MAIN_IFACE="eth0"
+HOSTNAME=`hostname`
+SHORT_HOSTNAME=`hostname -s`
+IP_CACHE="/var/cache/myip.txt"
+
+if [ -f $IP_CACHE ]; then
+    MAIN_IP=`cat $IP_CACHE`
+else
+    MAIN_IP=`wget -qqO - http://myip.dnsomatic.com`
+    echo $MAIN_IP > $IP_CACHE
+fi
+
+if ! `which iptables >/dev/null 2>&1`; then
+	if [[ -n "${SERVCONF_DEBUG}" ]]; then
+		echo "IPtables is not installed,aborting."
+	fi
+
+	exit 0
+fi
+
+if [[ ${MAIN_IP} == 10.* -o ${MAIN_IP} == 172.16.* -o ${MAIN_IP} == 192.168.* ]]; then
+
+	echo "NOT RUNNING the firewall on a private machine."
+	exit 0
+fi
+
 for script in ${IPTABLES_ROOT}/iptables.d/???_*.sh
 do
 	name=`basename ${script}`
