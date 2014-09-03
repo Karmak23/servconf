e8128ae7d1012474a22e3bcdfb67dede177ade7d
Olivier Cortès <oc@1flow.io>
2014-09-03 08:51:21 +0200

    disable part of the htoprc code, it doesn't work correctly

--- install-scripts/post-install.d/820_htop-config.sh
+++ install-scripts/post-install.d/820_htop-config.sh
@@ -2,12 +2,12 @@
 
 if [[ ! -L ~/.config/htop/htoprc ]]; then
 
-    mkdir -p ~${USER}/.config/htop
-    rm -rf ~${USER}/.config/htop/htoprc
-    ln -sf /etc/htoprc ~${USER}/.config/htop/htoprc
-    chown -R ${USER} ~${USER}/.config
+    #mkdir -p ~${USER}/.config/htop
+    #rm -rf ~${USER}/.config/htop/htoprc
+    #ln -sf /etc/htoprc ~${USER}/.config/htop/htoprc
+    #chown -R ${USER} ~${USER}/.config
 
     mkdir -p /root/.config/htop
     rm -rf /root/.config/htop/htoprc
-    ln -sf /etc/htoprc /root/.config/htop/htoprc
+    cp /etc/htoprc /root/.config/htop/htoprc
 fi
