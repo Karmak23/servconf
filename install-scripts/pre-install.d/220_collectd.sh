3a0743af1c2072fe29d7f25c5603895b71373527
Olivier Cortès <oc@1flow.io>
2014-09-03 08:06:23 +0200

    avoid an exit 0 to interrupt the whole install. not elegant but works.

--- install-scripts/pre-install.d/220_collectd.sh
+++ install-scripts/pre-install.d/220_collectd.sh
@@ -11,27 +11,32 @@ if [[ -e /etc/cron.d/munin-node ]]; then
     rm -f /etc/cron.d/munin-to-statsd
 fi
 
+function install_collectd() {
+    # TODO: move the installation/ppa block to sparks.
+    VERSION_INSTALLED=`dpkg -l | grep collectd | awk '{print $3}' | sort -u | grep 5.3`
+
+    if [[ -z "${VERSION_INSTALLED}" ]]; then
+        apt-add-repository -y ppa:optaros/collectd-5.3 || true
+        apt-get update -qq --yes --force-yes
+        apt-get install -qq collectd --yes --force-yes
+    fi
+    # END TODO
+
+    cat ${SERVCONF_PATH}/etc/collectd/collectd.conf.template \
+        | sed -e "s/@@CARBON_HOST@@/${CARBON_HOST}/" > /etc/collectd/collectd.conf
+
+    service collectd restart || service collectd start
+
+}
+
 if [[ -e ${MACHINE_PATH}/collectd.templates.variables ]]; then
+
 	source ${MACHINE_PATH}/collectd.templates.variables
+    install_collectd
 
 elif [[ -e ${GROUP_PATH}/collectd.templates.variables ]]; then
+
 	source ${GROUP_PATH}/collectd.templates.variables
-
-else
-	exit 0
+    install_collectd
 fi
 
-# TODO: move the installation/ppa block to sparks.
-VERSION_INSTALLED=`dpkg -l | grep collectd | awk '{print $3}' | sort -u | grep 5.3`
-
-if [[ -z "${VERSION_INSTALLED}" ]]; then
-    apt-add-repository -y ppa:optaros/collectd-5.3 || true
-    apt-get update -qq --yes --force-yes
-    apt-get install -qq collectd --yes --force-yes
-fi
-# END TODO
-
-cat ${SERVCONF_PATH}/etc/collectd/collectd.conf.template \
-    | sed -e "s/@@CARBON_HOST@@/${CARBON_HOST}/" > /etc/collectd/collectd.conf
-
-service collectd restart || service collectd start
