de1ce02a15fee24ea07623cb541364c0cf059256
Olivier Cortès <oc@1flow.io>
2014-09-10 22:02:06 +0200

    Use arptables if installed to disable all ARP trafic, it makes my servers IPs disabled by OVH.

--- iptables/iptables.d/001_reset.sh
+++ iptables/iptables.d/001_reset.sh
@@ -2,3 +2,16 @@
 
 iptables -F
 iptables -t nat -F
+
+if `which arptables >/dev/null 2>&1`; then
+
+
+	if [[ -z "${SERVCONF_FIREWALL_ARP_OPEN}" ]]; then
+		arptables -F
+		arptables -P INPUT DROP
+		arptables -P OUTPUT DROP
+		arptables -P FORWARD DROP
+	fi
+else
+	echo "ARP tables is not installed, not touching ARP."
+fi
