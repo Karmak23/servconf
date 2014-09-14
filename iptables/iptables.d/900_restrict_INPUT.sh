1e7781f545d2c5fc93d3eab2fbf41474ccc7a03f
Olivier Cortès <oc@1flow.io>
2014-09-14 18:07:29 +0200

    restart fail2ban if installed, else ou firewall clears it.

--- iptables/iptables.d/900_restrict_INPUT.sh
+++ iptables/iptables.d/900_restrict_INPUT.sh
@@ -3,3 +3,8 @@
 # Restrict *everything*, not just TCP.
 #iptables -A INPUT -i ${MAIN_IFACE} -s 0.0.0.0/0 -d ${MAIN_IP} -j REJECT
 iptables -A INPUT -i ${MAIN_IFACE} -s 0.0.0.0/0 -d 0.0.0.0/0 -j REJECT
+
+
+if [[ -e /etc/fail2ban/fail2ban.conf ]]; then
+	service fail2ban restart >/dev/null || true
+fi
