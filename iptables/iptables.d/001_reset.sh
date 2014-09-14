66d698cdc17ff339b16ede52a87e7c3bfd4d8abc
Olivier Cortès <oc@1flow.io>
2014-09-14 17:02:24 +0200

    implement arptables IP mangling to stop OVH spaming about misconfigured virtual IPs.

--- iptables/iptables.d/001_reset.sh
+++ iptables/iptables.d/001_reset.sh
@@ -3,21 +3,32 @@
 iptables -F
 iptables -t nat -F
 
+function maybe_stop_here() {
+	if [[ -n "${FIREWALL_OPEN}" ]]; then
+
+		echo "Firewall is OPEN in configuration. You are naked, bro!"
+		exit 0
+	fi
+}
+
 if `which arptables >/dev/null 2>&1`; then
 
+		arptables -F
+
+		maybe_stop_here
+
 	if [[ -z "${SERVCONF_FIREWALL_ARP_OPEN}" ]]; then
-		#arptables -F
-		#arptables -P INPUT DROP
-		#arptables -P OUTPUT DROP
-		#arptables -P FORWARD DROP
-		true
+
+		if [[ -n ${ARP_MANGLE_IPS} ]]; then
+			for VIRTUAL_IP in ${ARP_MANGLE_IPS}; do
+				arptables -A INPUT -d ${VIRTUAL_IP} -j DROP
+				arptables -A OUTPUT -s ${VIRTUAL_IP} -j mangle --mangle-ip-s ${MAIN_IP}
+			done
+		fi
 	fi
 else
 	echo "ARP tables is not installed, not touching ARP."
+
+	maybe_stop_here
 fi
 
-if [[ -n "${FIREWALL_OPEN}" ]]; then
-
-	echo "Firewall is OPEN in configuration. You are naked, bro!"
-	exit 0
-fi
