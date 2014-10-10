8984f319f8dcdfd2628db65cd8909fb80d9506c7
Olivier Cortès <oc@1flow.io>
2014-10-10 10:41:46 +0200

    rename variable and create one for deinstall to force package to not be installed.

--- install.sh
+++ install.sh
@@ -10,10 +10,15 @@ export SCRIPTS_BASE=${SERVCONF_PATH}/install-scripts
 export SERVCONF_COMMON=${SCRIPTS_BASE}/common.sh
 export GROUP_PATH=${SERVCONF_PATH}/private-data
 export MACHINE_PATH=${GROUP_PATH}/machines/${HOSTNAME}
-export HOST_INSTALL_FILE=${MACHINE_PATH}/install
+export MACHINE_INSTALL_FILE=${MACHINE_PATH}/install
+export MACHINE_DEINSTALL_FILE=${MACHINE_PATH}/deinstall
+# Not yet ready
+# export GROUP_INSTALL_FILE=${GROUP_PATH}/install
+# export GROUP_DEINSTALL_FILE=${GROUP_PATH}/deinstall
 
 export PREINST_BASE=${SCRIPTS_BASE}/pre-install.d
 export POSTINST_BASE=${SCRIPTS_BASE}/post-install.d
+
 
 # Make these key variables accessible to standalone-running scripts.
 cat <<EOF > /etc/servconf.conf
@@ -25,7 +30,11 @@ export SCRIPTS_BASE=${SERVCONF_PATH}/install-scripts
 export SERVCONF_COMMON=${SCRIPTS_BASE}/common.sh
 export GROUP_PATH=${SERVCONF_PATH}/private-data
 export MACHINE_PATH=${GROUP_PATH}/machines/${HOSTNAME}
-export HOST_INSTALL_FILE=${MACHINE_PATH}/install
+export MACHINE_INSTALL_FILE=${MACHINE_PATH}/install
+export MACHINE_DEINSTALL_FILE=${MACHINE_PATH}/deinstall
+# Not yet ready
+# export GROUP_INSTALL_FILE=${GROUP_PATH}/install
+# export GROUP_DEINSTALL_FILE=${GROUP_PATH}/deinstall
 
 export PREINST_BASE=${SCRIPTS_BASE}/pre-install.d
 export POSTINST_BASE=${SCRIPTS_BASE}/post-install.d
