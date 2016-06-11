03f18d2173382957805a2eab25024e0111932ad9
Olivier Cortès <oc@1flow.io>
2016-06-11 07:40:12 +0200

    still firewall fixes.

--- iptables/iptables.d/040_open_ports.sh
+++ iptables/iptables.d/040_open_ports.sh
@@ -4,7 +4,7 @@ if [[ -n "${OPEN_PORTS_TCP}" ]]; then
 
     for PORT in ${OPEN_PORTS_TCP}
     do
-        iptables -A INPUT -p tcp -m tcp -i ${MAIN_IFACE} -s 0.0.0.0 -d 0.0.0.0 \
+        iptables -A INPUT -p tcp -m tcp -i ${MAIN_IFACE} -s 0.0.0.0/0 -d 0.0.0.0/0 \
                 --dport ${PORT} -j ACCEPT
     done
 fi
@@ -13,7 +13,7 @@ if [[ -n "${OPEN_PORTS_UDP}" ]]; then
 
     for PORT in ${OPEN_PORTS_UDP}
     do
-        iptables -A INPUT -p udp -i ${MAIN_IFACE} -s 0.0.0.0 -d 0.0.0.0 \
+        iptables -A INPUT -p udp -i ${MAIN_IFACE} -s 0.0.0.0/0 -d 0.0.0.0/0 \
                 --dport ${PORT} -j ACCEPT
     done
 fi
